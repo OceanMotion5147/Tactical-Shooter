7eca1c6204c03e6a90914364c8ccbf33faf39ce1
OceanMotion5147 <aidbro36@kingshigh.school.nz>
2022-04-08 10:44:52 +1200

    Fixed new fog code, extended size of original fog

--- Scripts/Test.gd
+++ Scripts/Test.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 const LightTexture = preload("res://Resources/Pictures/Light.png")
-const GRID_SIZE = 64
+const GRID_SIZE = 48
 
 onready var fog = $Fog
 
@@ -14,8 +14,8 @@ var lightImage = LightTexture.get_data()
 var light_offset = Vector2(LightTexture.get_width()/2, LightTexture.get_height()/2)
 
 func _ready():
-	var fog_image_width = display_width/GRID_SIZE
-	var fog_image_height = display_height/GRID_SIZE
+	var fog_image_width = display_width
+	var fog_image_height = display_height
 	fogImage.create(fog_image_width, fog_image_height, false, Image.FORMAT_RGBAH)
 	fogImage.fill(Color.black)
 	lightImage.convert(Image.FORMAT_RGBAH)
