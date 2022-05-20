d767341956034c3da82d28d754ff356401382edd
OceanMotion5147 <aidbro36@kingshigh.school.nz>
2022-05-20 12:18:10 +1200

    Small Changes

---
+++ Scripts/BulletManager.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+
+func handle_bullet_spawned(bullet):
+	add_child(bullet)
+
--- Scripts/Test.gd
+++ Scripts/Test.gd
@@ -4,6 +4,10 @@ const LightTexture = preload("res://Resources/Pictures/Light.png")
 const GRID_SIZE = 48
 
 onready var fog = $Fog
+onready var player = $BulletManager
+onready var bullet_manager = $BulletManager
+
+
 
 var display_width = ProjectSettings.get("display/window/size/width")
 var display_height = ProjectSettings.get("display/window/size/height")
@@ -13,7 +17,8 @@ var fogTexture = ImageTexture.new()
 var lightImage = LightTexture.get_data()
 var light_offset = Vector2(LightTexture.get_width()/2, LightTexture.get_height()/2)
 
-func _ready():
+func _ready() -> void:
+	player.connect("player_fired_bullet", bullet_manager, "handle_bullet_spawned")
 	var fog_image_width = display_width
 	var fog_image_height = display_height
 	fogImage.create(fog_image_width, fog_image_height, false, Image.FORMAT_RGBAH)
@@ -38,3 +43,5 @@ func update_fog_image_texture():
 
 func _input(event):
 	update_fog(get_local_mouse_position()/GRID_SIZE)
+
+
