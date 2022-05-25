02b3b390847f0f61f8680385ceb958f139ed9e08
OceanMotion5147 <aidbro36@kingshigh.school.nz>
2022-05-26 09:55:15 +1200

    Gun now "Shoots" properly

--- Scripts/Test.gd
+++ Scripts/Test.gd
@@ -1,47 +1,12 @@
 extends Node2D
 
-const LightTexture = preload("res://Resources/Pictures/Light.png")
-const GRID_SIZE = 48
 
-onready var fog = $Fog
-onready var player = $BulletManager
 onready var bullet_manager = $BulletManager
+onready var player = $Player
 
 
-
-var display_width = ProjectSettings.get("display/window/size/width")
-var display_height = ProjectSettings.get("display/window/size/height")
-
-var fogImage = Image.new()
-var fogTexture = ImageTexture.new()
-var lightImage = LightTexture.get_data()
-var light_offset = Vector2(LightTexture.get_width()/2, LightTexture.get_height()/2)
 
 func _ready() -> void:
 	player.connect("player_fired_bullet", bullet_manager, "handle_bullet_spawned")
-	var fog_image_width = display_width
-	var fog_image_height = display_height
-	fogImage.create(fog_image_width, fog_image_height, false, Image.FORMAT_RGBAH)
-	fogImage.fill(Color.black)
-	lightImage.convert(Image.FORMAT_RGBAH)
-	fog.scale *= GRID_SIZE
-
-func update_fog(new_grid_position):
-	fogImage.lock()
-	lightImage.lock()
-	
-	var light_rect = Rect2(Vector2.ZERO, Vector2(lightImage.get_width(), lightImage.get_height()))
-	fogImage.blend_rect(lightImage, light_rect, new_grid_position - light_offset)
-	
-	fogImage.unlock()
-	lightImage.unlock()
-	update_fog_image_texture()
-
-func update_fog_image_texture():
-	fogTexture.create_from_image(fogImage)
-	fog.texture = fogTexture
-
-func _input(event):
-	update_fog(get_local_mouse_position()/GRID_SIZE)
 
 
