25a38570044216bc72f40ac5a15d90116a90a558
OceanMotion5147 <aidbro36@kingshigh.school.nz>
2022-06-23 10:54:43 +1200

    Enemy now Moves but doesnt face same direction
    Collision shapes around player and enemy changed to polygons so they are more "realistic"

--- Scripts/AI.gd
+++ Scripts/AI.gd
@@ -11,18 +11,30 @@ enum State{
 
 
 onready var player_detection_zone = $PlayerDetectionZone
+onready var patrol_timer = $PatrolTimer
 
 
 var current_state: int = State.PATROL setget set_state
-var actor = null
+var actor: KinematicBody2D = null
 var player: Player = null
 var weapon: Weapon = null
+
+
+var origin: Vector2 = Vector2.ZERO
+var patrol_location: Vector2 = Vector2.ZERO
+var patrol_location_reached: bool = false
+var actor_velocity: Vector2 = Vector2.ZERO
 
 
 func _process(delta: float) -> void:
 	match current_state:
 		State.PATROL:
-			pass
+			if not patrol_location_reached:
+				actor.move_and_slide(actor_velocity)
+				if actor.global_position.distance_to(patrol_location) < 5:
+					patrol_location_reached = true
+					actor_velocity = Vector2.ZERO
+					patrol_timer.start()
 		State.ENGAGE:
 			if player != null and weapon != null:
 				var angle_to_player = actor.global_position.direction_to(player.global_position).angle()
@@ -44,6 +56,11 @@ func set_state(new_state: int):
 	if new_state == current_state:
 		return
 		
+	if new_state == State.PATROL:
+		origin = actor.global_position
+		patrol_timer.start()
+		patrol_location_reached = true
+		
 	current_state = new_state
 	emit_signal("state_changed", current_state)
 
@@ -52,3 +69,18 @@ func _on_PlayerDetectionZone_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
 		set_state(State.ENGAGE)
 		player = body
+
+
+func _on_PlayerDetectionZone_body_exited(body):
+	if player and body == player:
+		set_state(State.PATROL)
+		player = null
+
+
+func _on_PatrolTimer_timeout():
+	var patrol_range = 150
+	var random_x = rand_range(-patrol_range, patrol_range)
+	var random_y = rand_range(-patrol_range, patrol_range)
+	patrol_location = Vector2(random_x, random_y) + origin
+	patrol_location_reached = false
+	actor_velocity = actor.global_position.direction_to(patrol_location) * 100
