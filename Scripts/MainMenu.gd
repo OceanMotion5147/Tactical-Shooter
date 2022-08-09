db55858ec013aeb153a168094b1938d49f9d8bad
OceanMotion5147 <aidbro36@kingshigh.school.nz>
2022-08-10 09:56:20 +1200

    Opions menu Finished, Code still not working

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,15 +1,18 @@
 extends Control
 
+onready var options_menu = $OptionsMenu
+onready var play_btn = $VBoxContainer/PlayButton
+
 func _ready():
-	$VBoxContainer/PlayButton.grab_focus()
-	
+	play_btn.grab_focus()
+
 
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://SCenes/Test.tscn")
 
 
 func _on_OptionsButton_pressed():
-	get_tree().change_scene("res://SCenes/OptionsMenu.tscn")
+	options_menu.popup_centered()
 
 
 func _on_QuitButton_pressed():
--- Scripts/OptionsMenu.gd
+++ Scripts/OptionsMenu.gd
@@ -21,7 +21,7 @@ onready var mouse_sens_amount = $SettingTabs/GamePlay/MarginContainer3/GameplayS
 onready var mouse_slider = $SettingTabs/GamePlay/MarginContainer3/GameplaySettings/MouseSensOption/MouseSlider
 
 func _ready():
-
+	
 	display_options.select(1 if Save.game_data.fullscreen_on else 0)
 	GlobalSettings.toggle_fullscreen(Save.game_data.fullscreen_on)
 	vsync_btn.pressed = Save.game_data.vsync_on
@@ -36,4 +36,43 @@ func _ready():
 	mouse_slider.value = Save.game_data.mouse_sens
 	
 	
+
+
+func _on_DisplayOptions_item_selected(index):
+	GlobalSettings.toggle_fullscreen(true if index == 1 else false)
+	
+	
+func _on_VsyncBtn_toggled(button_pressed):
+	GlobalSettings.toggle_vsync(button_pressed)
+
+
+func _on_DisplayFpsBtn_toggled(button_pressed):
+	GlobalSettings.toggle_fps_display(button_pressed)
+
+
+func _on_MaxFpsSlider_value_changed(value):
+	GlobalSettings.set_max_fps(value)
+	max_fps_val.text = str(value) if value < max_fps_slider.max_value else "max"
+
+
+func _on_BloomBtn_toggled(button_pressed):
+	GlobalSettings.toggle_bloom(button_pressed)
+
+
+func _on_BrightnessSlider_value_changed(value):
+	GlobalSettings.update_brightness(value)
+
+
+func _on_MasterSlider_value_changed(value):
+	GlobalSettings.update_master_vol(value)
+
+
+func _on_FovSlider_value_changed(value):
+	GlobalSettings.update_fov(value)
+	fov_amount.text = str(value)
+
+
+func _on_MouseSlider_value_changed(value):
+	GlobalSettings.update_mouse_sens(value)
+	mouse_sens_amount.text = str(value)
 
