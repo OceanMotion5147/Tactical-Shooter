ade466b8485b53303138b7ab1bb0e865a165d5a9
OceanMotion5147 <aidbro36@kingshigh.school.nz>
2022-08-22 12:20:17 +1200

    continuation of options menu coding

--- Scripts/GlobalSettings.gd
+++ Scripts/GlobalSettings.gd
@@ -1,6 +1,7 @@
 extends Node
 
 signal bloom_toggled(value)
+signal fov_updated(value)
 
 func toggle_fullscreen(value):
 	OS.window_fullscreen = value
@@ -12,3 +13,19 @@ func toggle_vsycn(value):
 
 func toggle_bloom(value):
 	emit_signal("bloom_toggled", value)
+	
+	
+func update_master_val(vol):
+	AudioServer.set_bus_volume_db(0, vol)
+	
+	
+func update_music_val(vol):
+	AudioServer.set_bus_volume_db(1, vol)
+	
+	
+func update_SFX_val(vol):
+	AudioServer.set_bus_volume_db(2, vol)
+	
+	
+func update_fov(value):
+	emit_signal("fov_updated", value)
